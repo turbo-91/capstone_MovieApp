498226db16c9981ab780c8a85409303664a734b6
Torben Jost <torbenjost@proton.me>
2025-01-27 12:46:02 +0100

    create Movie model

---
+++ backend/src/main/java/org/example/backend/model/Movie.java
@@ -0,0 +1,25 @@
+package org.example.backend.model;
+
+public record Movie(
+        int id,
+        String slug,
+        String title,
+        String year,
+        String overview,
+        String imgUrl
+) {
+
+    public Movie(int id, String slug, String title, String year, String overview, String imgUrl) {
+        this.id = id;
+        this.slug = slug;
+        this.title = title;
+        this.year = year;
+        this.overview = overview;
+        this.imgUrl = imgUrl;
+    }
+
+    public Movie withId(int id) {
+        return new Movie(id, this.slug, this.title, this.year, this.overview, this.imgUrl);
+    }
+
+}
