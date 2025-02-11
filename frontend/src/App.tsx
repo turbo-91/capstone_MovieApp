17d7347bdc2b6184da4d12da209d9d6edd965b8e
Torben Jost <torbenjost@proton.me>
2025-02-11 11:30:04 +0100

    add MovieDetail component on movie of the day click

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,53 @@
 import useSWR from "swr";
-import {fetcher} from "./utils/fetcher.ts";
-import {IMovie} from "./types/Movie.ts";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import {useEffect, useState} from "react";
+import { fetcher } from "./utils/fetcher.ts";
+import { IMovie } from "./types/Movie.ts";
+import { useEffect, useState } from "react";
+import MovieDetail from "./components/MovieDetail.tsx";
 
 function App() {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState<IMovie[]>([]);
     const { data, error } = useSWR("api/movies/daily", fetcher, {
         shouldRetryOnError: false,
     });
 
+    const [selectedMovie, setSelectedMovie] = useState<IMovie | null>(null);
+
     useEffect(() => {
-        if (data && !movies.length) {
+        if (data && movies.length === 0) {
             setMovies(data);
         }
     }, [data, movies]);
 
     if (!data && !error) return <div>Loading...</div>;
     if (error) return <div>Error loading movies: {error.message}</div>;
-    if (!data.length) return <p>No movies found.</p>;
-
+    if (!data?.length) return <p>No movies found.</p>;
 
     return (
         <div className="app">
-                    {data.map((movie: IMovie) => (
-                            <><h2>{movie.title}</h2>
-                                <p>{movie.year}</p>
-                                <h2>{movie.regisseur}</h2>
-                                <p>{movie.stars}</p>
-                                <img src={movie.imgImdb} alt={movie.title} width="200" /></>
-                        ))}
+            {selectedMovie ? (
+                <MovieDetail
+                    movie={selectedMovie}
+                    onBack={() => setSelectedMovie(null)}
+                />
+            ) : (
+                data.map((movie: IMovie) => (
+                    <div
+                        key={movie.netzkinoId}
+                        onClick={() => setSelectedMovie(movie)}
+                        onKeyUp={() => setSelectedMovie(movie)}
+                        role="button"
+                        tabIndex={0}
+                    >
+                        <h2>{movie.title}</h2>
+                        <p>{movie.year}</p>
+                        <h2>{movie.regisseur}</h2>
+                        <p>{movie.stars}</p>
+                        <img src={movie.imgImdb} alt={movie.title} width="200" />
+                    </div>
+                ))
+            )}
         </div>
     );
 }
+
 export default App;
