3a7926a3f8220988226667778ae76242f4ad78e9
Torben Jost <torbenjost@proton.me>
2025-02-11 11:47:36 +0100

    add Layout in Form of a Header with NavBar on click

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,22 @@
-import useSWR from "swr";
-import { fetcher } from "./utils/fetcher.ts";
-import { IMovie } from "./types/Movie.ts";
-import { useEffect, useState } from "react";
-import MovieDetail from "./components/MovieDetail.tsx";
+import Layout from "./components/layout/Layout.tsx";
+import {Route, Routes} from "react-router-dom";
+import MoviesOfTheDay from "./components/MoviesOfTheDay.tsx";
 
 function App() {
-    const [movies, setMovies] = useState<IMovie[]>([]);
-    const { data, error } = useSWR("api/movies/daily", fetcher, {
-        shouldRetryOnError: false,
-    });
 
-    const [selectedMovie, setSelectedMovie] = useState<IMovie | null>(null);
-
-    useEffect(() => {
-        if (data && movies.length === 0) {
-            setMovies(data);
-        }
-    }, [data, movies]);
-
-    if (!data && !error) return <div>Loading...</div>;
-    if (error) return <div>Error loading movies: {error.message}</div>;
-    if (!data?.length) return <p>No movies found.</p>;
 
     return (
-        <div className="app">
-            {selectedMovie ? (
-                <MovieDetail
-                    movie={selectedMovie}
-                    onBack={() => setSelectedMovie(null)}
-                />
-            ) : (
-                data.map((movie: IMovie) => (
-                    <div
-                        key={movie.netzkinoId}
-                        onClick={() => setSelectedMovie(movie)}
-                        onKeyUp={() => setSelectedMovie(movie)}
-                        role="button"
-                        tabIndex={0}
-                    >
-                        <h2>{movie.title}</h2>
-                        <p>{movie.year}</p>
-                        <h2>{movie.regisseur}</h2>
-                        <p>{movie.stars}</p>
-                        <img src={movie.imgImdb} alt={movie.title} width="200" />
-                    </div>
-                ))
-            )}
-        </div>
+            <Layout>
+                <main>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<MoviesOfTheDay/>}
+
+                        />
+                    </Routes>
+                </main>
+            </Layout>
     );
 }
 
---
+++ frontend/src/components/layout/NavBar.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NavBar() {
+    return (
+        <div>
+            <a href="/">Home</a>
+            <a href="/login">Login</a>
+        </div>
+    );
+}
