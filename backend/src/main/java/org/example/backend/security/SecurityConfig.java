12d453a1e2359a9dc57e913d898998790374f6c1
Torben Jost <torbenjost@proton.me>
2025-02-13 16:09:19 +0100

    restart login logic

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -2,34 +2,31 @@ package org.example.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)  // normalerweise erzeugt dies eine Sicherheitslücke, Achtung!
-
-                .authorizeHttpRequests(authorize -> authorize
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
                         .anyRequest().permitAll()
                 )
-                .logout(logout -> logout.logoutUrl("/api/users/logout")
-                        .logoutSuccessHandler((request, response, authentification) -> response.setStatus(200))
-                )
-                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .oauth2Login(withDefaults());
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(o -> o.defaultSuccessUrl("/localhost:5173"));
         return http.build();
+
     }
 
 }
--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -12,40 +12,12 @@ export default function Header(props: HeaderProps) {
     const {user, setUser} = props;
     const [menu, setMenu] = useState<boolean>(false);
 
-    function login() {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
-        window.open(host + '/oauth2/authorization/github', '_self')
-
-    }
-
-    useEffect(() => {
-        getUserAxios(); // Update user state when component loads
-    }, []);
-
-    function getUserAxios() {
-        axios.get("api/users/me").then((response) => {
-            console.log("axios ", response.data);
-            setUser(response.data);
-        }).catch(() => {
-            setUser(undefined); // Ensure user state is cleared if request fails
-        });
-    }
-
-    function logout() {
-        axios.post("api/users/logout").then(() => {
-            setUser(undefined); // Clear user state on logout
-            setMenu(false); // Ensure menu is closed on logout
-        });
-    }
 
 
 
     return (
         <div>
             <h1>MovieApp</h1>
-            <p>{user?.githubId}</p>
-            <button onClick={login}>Login</button>
-            <button onClick={logout}>Logout</button>
             {user && (
                 <>
                     <h1 onClick={() => setMenu((prevMenu) => !prevMenu)}>Menu</h1>
