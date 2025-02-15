aeebf119d329ad0ffa8a81e6d535f58165075e24
Torben Jost <torbenjost@proton.me>
2025-02-15 11:30:36 +0100

    fix security concern

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
 
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
